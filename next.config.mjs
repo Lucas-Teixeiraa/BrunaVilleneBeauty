8bcf562c33a5f4ad4a2270fa81b5cde0d75423fe
Lucas <lteixeira507@gmail.com>
2025-04-11 21:48:59 -0400

    /fix: next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ooutput: 'export',
+  output: 'export',
   basePath: '/BrunaVilleneBeauty', 
   assetPrefix: '/BrunaVilleBeauty/',
   images: {
